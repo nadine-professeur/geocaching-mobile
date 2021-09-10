8eaa4212d4e1ab1d8dd0eed56c0c50b31c28f2d3
nadine-professeur <nadine.giasson.st-amand@cegep-rimouski.qc.ca>
2021-09-10 12:00:55 -0400

    Initialisation du projet

---
+++ app/src/main/java/com/ludijeu/bibliotheque/VueBibliotheque.java
@@ -0,0 +1,13 @@
+package com.ludijeu.bibliotheque;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueBibliotheque extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_bibliotheque);
+    }
+}
